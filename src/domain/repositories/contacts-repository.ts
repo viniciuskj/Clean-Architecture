e0494d92a2b82c1402f0037532bab32004eacd45
viniciuskj <vinicius.koe@puccampinas.edu.br>
2025-05-02 10:18:54 -0300

    test: contact-repository

---
+++ src/domain/repositories/contacts-repository.ts
@@ -0,0 +1,43 @@
+import { ContactDataSource } from "../../../src/data/interfaces/data-sources/contact-data-source";
+import { Contact } from "../../../src/domain/entities/contact";
+import { ContactRepository } from "../../../src/domain/interfaces/repositories/contact-repository";
+import { ContactRepositoryImpl } from "../../../src/domain/repositories/contact-repository";
+
+class MockContactDataSource implements ContactDataSource {
+    create(contact: Contact): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    getAll(): Promise<Contact[]> {
+        throw new Error("Method not implemented.");
+    }
+}
+
+describe("Contact Repository", () => {
+    let mockContactDataSource: ContactDataSource;
+    let contactRepository: ContactRepository
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        mockContactDataSource = new MockContactDataSource()
+        contactRepository = new ContactRepositoryImpl(mockContactDataSource)
+    })
+
+    describe("getAllContacts", () => {
+        test("should return data", async () => {
+            const expectedData = [{ id: "1", surname: "Smith", firstName: "John", email: "john@gmail.com" }]
+            jest.spyOn(mockContactDataSource, "getAll").mockImplementation(() => Promise.resolve(expectedData))
+            const result = await contactRepository.getContacts();
+            expect(result).toBe(expectedData)
+        });
+    })
+
+    describe("createContact", () => {
+        test("should return true", async () => {
+            const inputData = { id: "1", surname: "Smith", firstName: "John", email: "john@gmail.com" }
+            jest.spyOn(mockContactDataSource, "create").mockImplementation(() => Promise.resolve(true))
+            const result = await contactRepository.createContact(inputData);
+            expect(result).toBe(true)
+        });
+    })
+
+})
